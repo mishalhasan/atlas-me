2bc848e5680b2a4877bb5dcac8d45500646a5635
mishalhasan <mishalhasan2198@gmail.com>
2026-05-08 11:04:12 +0300

    feat: added cascade on delete

--- server/models/initModels.js
+++ server/models/initModels.js
@@ -9,7 +9,7 @@ const db = {
 
 // Define relationships between models (after initialization to avoid circular dependency)
 const { User, Pin } = db;
-User.hasMany(Pin, { foreignKey: "userId" });
+User.hasMany(Pin, { foreignKey: "userId", onDelete: "CASCADE"});
 Pin.belongsTo(User, { foreignKey: "userId" });
 
 // Attach sequelize instance for syncing / connection access
